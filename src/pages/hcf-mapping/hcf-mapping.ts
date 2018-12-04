f448bbf01312cc0cd41873e6b40ea3508ce79db2
admin <admin@470-17.local>
2018-12-04 14:07:27 +0800

    First commit

---
+++ src/pages/hcf-mapping/hcf-mapping.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the HcfMappingPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-hcf-mapping',
+  templateUrl: 'hcf-mapping.html',
+})
+export class HcfMappingPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HcfMappingPage');
+  }
+
+}
---
+++ src/pages/request-visualization/request-visualization.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the RequestVisualizationPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-request-visualization',
+  templateUrl: 'request-visualization.html',
+})
+export class RequestVisualizationPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad RequestVisualizationPage');
+  }
+
+}

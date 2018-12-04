f9c2c6b3f8eaa71f9689cd6244f6ef1d791e93e3
jesmrie <jtorrepedrola@gmail.com>
2018-12-04 16:11:05 +0800

    Responder

--- src/pages/respond-to-request/respond-to-request.ts
+++ src/pages/respond-to-request/respond-to-request.ts
@@ -22,4 +22,8 @@ export class RespondToRequestPage {
     console.log('ionViewDidLoad RespondToRequestPage');
   }
 
+  pushRequestVisualizationPage(){
+    this.navCtrl.push('RequestVisualizationPage');
+  }
+
 }

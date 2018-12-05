0f58d325f38afa6a3720c2f591378829c304c951
njompad <admin@470-20.local>
2018-12-05 16:55:06 +0800

    nj omg maps

--- src/pages/hcf-mapping/hcf-mapping.ts
+++ src/pages/hcf-mapping/hcf-mapping.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import leaflet from 'leaflet';
+import { LoginPage } from '../login/login';
 /**
  * Generated class for the HcfMappingPage page.
  *
@@ -39,6 +40,10 @@ export class HcfMappingPage {
     // }).on('locationfound', (e) => {
     //   console.log('found you');
     //   })
+    var planes = [
+      ["7C6B07",10.3502881,123.8988732],
+      ["7C6B38",10.361011,123.9070701],
+      ];
     this.map = leaflet.map("map").fitWorld();
     leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -46,18 +51,35 @@ export class HcfMappingPage {
     }).addTo(this.map);
     this.map.locate({
       setView: true,
-      maxZoom: 10
+      maxZoom: 15
     }).on('locationfound', (e) => {
       let markerGroup = leaflet.featureGroup();
       let marker: any = leaflet.marker([e.latitude, e.longitude]).on('click', () => {
-        alert('Marker clicked');
+        alert('You are here');
+        console.log(e.latitude,e.longitude);
       })
       markerGroup.addLayer(marker);
       this.map.addLayer(markerGroup);
+      var circle = leaflet.circle([e.latitude, e.longitude], {
+        color: 'Green',
+            fillColor: '#81C784',
+          fillOpacity: 0.5,
+          radius: 2000
+      }).addTo(this.map);
+      leaflet.marker([10.3502881,123.8988732]).on('click', () => {
+        alert('Need Help');
+        console.log(e.latitude,e.longitude);
+      }).addTo(this.map);
+      leaflet.marker([10.361011,123.9070701]).on('click', () => {
+        alert('NJ');
+        console.log(e.latitude,e.longitude);
+      }).addTo(this.map);
       }).on('locationerror', (err) => {
         alert(err.message);
     })
   }
+  // 10.3502881,123.8988732
+  // 10.361011,123.9070701
 
   PushReportEventPage(){
     this.navCtrl.push('EventReportPage');

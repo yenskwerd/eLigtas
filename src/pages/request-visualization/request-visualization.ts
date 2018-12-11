3e7c167e93f901350c477ef214c5dea3f09f99b5
yenskwerd <admin@470-11.local>
2018-12-11 13:37:50 +0800

    asd

--- src/pages/request-visualization/request-visualization.ts
+++ src/pages/request-visualization/request-visualization.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, AlertController,  NavController, NavParams } from 'ionic-angular';
 import leaflet from 'leaflet';
 
 /**
@@ -18,7 +18,7 @@ export class RequestVisualizationPage {
   @ViewChild('map') mapContainer:ElementRef;
   map:any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
@@ -51,7 +51,7 @@ export class RequestVisualizationPage {
     }).addTo(this.map);
     this.map.locate({
       setView: true,
-      maxZoom: 10
+      maxZoom: 15
     }).on('locationfound', (e) => {
       let markerGroup = leaflet.featureGroup();
       let marker: any = leaflet.marker([e.latitude, e.longitude]).on('click', () => {
@@ -62,10 +62,69 @@ export class RequestVisualizationPage {
       }).on('locationerror', (err) => {
         alert(err.message);
     })
-  }
+    leaflet.marker([10.3502881,123.8988732]).on('click', () => {
+      leaflet.marker([10.3502881,123.8988732], {icon: greenIcon}).addTo(this.map);
+      if(this.responseConfirm()==false){
+      console.log(this.ret);
+      leaflet.marker([10.3502881,123.8988732], {icon: blueIcon}).addTo(this.map);
+    }
+    }).bindPopup("Need help").addTo(this.map);
+    var greenIcon = new leaflet.Icon({
+      iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
+    });
+    var blueIcon = new leaflet.Icon({
+      iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41]
+    });
 
+    
+    
+    
+  }
+  
+  ret:any;
   pushRespondToRequest(){
     this.navCtrl.push('RespondToRequestPage');
   }
+  responseConfirm(): boolean {
+    
+    let alert = this.alertCtrl.create({
+
+      title: 'Confirm response',
+      message: 'Do you want help this person?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+            this.ret = false;
+            console.log(this.ret);
+          }
+        },
+        {
+          text: 'Help',
+          handler: () => {
+            console.log('Please hurry!');
+            this.ret = true;
+          }
+        }
+      ]
+    });
+    console.log(this.ret);
+    alert.present();
+    return this.ret;
+  }
 
 }
+
+

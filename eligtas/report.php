4bebbefb3faf751de0114984bc0e93a30b60f222
jesmrie <jtorrepedrola@gmail.com>
2018-12-11 18:07:50 +0800

    jesmrieeeee

---
+++ eligtas/report.php
@@ -0,0 +1,54 @@
+<?php
+
+   // Define database connection parameters
+   $hn      = 'localhost';
+   $un      = 'root';
+   $pwd     = '';
+   $db      = 'eligtas';
+   $cs      = 'utf8';
+
+
+   // Set up the PDO parameters
+   $dsn  = "mysql:host=" . $hn . ";port=3306;dbname=" . $db . ";charset=" . $cs;
+   $opt  = array(
+                        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+                        PDO::ATTR_EMULATE_PREPARES   => false,
+                       );
+   // Create a PDO instance (connect to the database)
+   $pdo  = new PDO($dsn, $un, $pwd, $opt);
+   $data = array();
+   $data2 = file_get_contents("php://input");
+   if(isset($data2)){
+      $request = json_decode($data2);
+      $request_type_id = $request->request_type_id;
+      $person_to_check = $request->person_to_check;
+  
+      $persons_injured = $request->persons_injured;
+      $persons_trapped = $request->persons_trapped;
+      $other_info = $request->other_info;
+      // $request_lat = $request->request_lat;
+      // $request_long = $request->request_long;
+   }
+   
+
+      
+   // Attempt to query database table and retrieve data
+   try {
+      $stmt= $pdo->query('INSERT INTO request (request_type_id, person_to_check,persons_injured, persons_trapped, other_info) VALUES("'.$request_type_id.'","'.$person_to_check.'","'.$persons_injured.'","'.$persons_trapped.'","'.$other_info.'")');
+      
+      while($row  = $stmt->fetch(PDO::FETCH_OBJ))
+      {
+         // Assign each row of data to associative array
+         $data[] = $row;
+      }
+
+      // Return data as JSON
+      echo json_encode("test");
+   }
+   catch(PDOException $e)
+   {
+      echo $e->getMessage();
+   }
+
+?>
--- src/pages/event-report/event-report.ts
+++ src/pages/event-report/event-report.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import {Http, Headers, RequestOptions}  from '@angular/http';
+import 'rxjs/add/operator/map';
+import { elementProperty } from '@angular/core/src/render3/instructions';
 
 /**
  * Generated class for the EventReportPage page.
@@ -15,11 +18,87 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class EventReportPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl:AlertController,private http: Http) {
   }
+
+  @ViewChild('event') event;
+  @ViewChild('persons_injured') persons_injured;
+  @ViewChild('persons_trapped') persons_trapped;
+  @ViewChild('other_info') other_info;
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EventReportPage');
+  }
+
+  report(){
+    if(this.persons_injured.value==""){
+        
+          let alert = this.alertCtrl.create({
+            message:"Persons injured field is empty!",
+            buttons: ['OK']
+          
+          });
+          
+          alert.present();
+        
+        } else if(this.persons_trapped.value==""){
+        
+          let alert = this.alertCtrl.create({
+            message:"Persons trapped field is empty!",
+            buttons: ['OK']
+  
+          });
+          
+          alert.present();
+         
+      }else {
+        var headers = new Headers();
+      
+        headers.append("Accept", 'application/json');
+        headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        headers.append('Access-Control-Allow-Origin' , '*');
+        headers.append('Access-Control-Allow-Headers' , 'Content-Type');
+        headers.append('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT');
+        
+        
+        let options = new RequestOptions({ headers: headers });
+        let data = {
+          request_type_id : 1,
+          person_to_check : null,
+          event: null,
+          persons_injured: this.persons_injured.value,
+          persons_trapped: this.persons_trapped.value,
+          other_info: this.other_info.value
+        }
+        console.log(data);
+        this.http.post('http://localhost/eligtas/report.php', data, options)
+        .map(res=> res.json())
+        .subscribe((data: any) =>
+        {
+           // If the request was successful notify the user
+           console.log(data);
+           let alert = this.alertCtrl.create({
+            title:"CONGRATS",
+            subTitle: "You successfully changed your password!",
+            buttons: ['OK']
+            }); 
+
+            alert.present();
+            //this.navCtrl.setRoot('PilgrimProfilePage'); 
+        },
+        (error : any) =>
+        {
+          console.log(error);
+          let alert2 = this.alertCtrl.create({
+            title:"FAILED",
+            subTitle: "Wrong input/s, try again!",
+            buttons: ['OK']
+            });
+
+        alert2.present();
+        });
+
+      }
   }
 
 }

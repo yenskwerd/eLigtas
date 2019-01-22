bffc1c5bad1799c9b7ef1c97f9f66ce69bdf190f
yenskwerd <lumienemae@gmail.com>
2019-01-22 10:00:51 +0800

    report

--- src/pages/modal/modal.ts
+++ src/pages/modal/modal.ts
@@ -20,7 +20,6 @@ export class ModalPage {
 
   @ViewChild('victim_name') victim_name;
   @ViewChild('victim_desc') victim_desc;
-  @ViewChild('other_info') other_info;
 
   desc: any;
   triage: any;
@@ -33,7 +32,13 @@ export class ModalPage {
   }
 
   public closeModal(){
-    this.viewCtrl.dismiss();
+    let data = { 
+      victim_name: this.victim_name.value,
+      victim_desc: this.victim_desc.value,
+      triage: this.triage
+    }
+    this.viewCtrl.dismiss(data);
+    console.log(data);
   }
 
 
--- src/pages/report/report.ts
+++ src/pages/report/report.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ModalController, IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Http, Headers, RequestOptions}  from '@angular/http';
+import { HttpClient } from '@angular/common/http'; 
 
 /**
  * Generated class for the ReportPage page.
@@ -15,18 +17,60 @@ import { ModalController, IonicPage, NavController, NavParams } from 'ionic-angu
 })
 export class ReportPage {
 
-  items: any = ["asd", "asd", "asd"];
+  public items : Array<any> = [];
+  reports: any = [];
+  value: any;
+  hello: any;
 
-  constructor(public modalCtrl: ModalController, public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public modalCtrl: ModalController, private http: HttpClient, public navCtrl: NavController, public navParams: NavParams) {
+    this.hello = navParams.data.data;
   }
 
   public openModal(){ 
-    var modalPage = this.modalCtrl.create('ModalPage'); 
+    var modalPage = this.modalCtrl.create('ModalPage');
+    modalPage.onDidDismiss(data => {
+      console.log(data);
+      this.value=data;//here
+      console.log(JSON.stringify(this.value,undefined,2));
+      console.log(this.hello);
+    });
     modalPage.present(); 
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ReportPage');
+    this.load();
+  }
+
+
+  load() : void
+  {
+     this.http
+     .get('http://usc-dcis.com/eligtas.app/retrieve-report.php')
+     .subscribe((data : any) =>
+     {
+        console.dir(data);
+        this.items = data;
+        console.log("this: " + this.items);
+        this.generateItems(data);
+     },
+     (error : any) =>
+     {
+        console.dir(error);
+     });
+  }
+
+  generateItems(data) {
+    for (let i=0; i < data.length; i++) {
+      if(data[i].victim_name!="") {
+        this.reports.push(
+          { victim_name: data[i].victim_name,
+            victim_desc: data[i].victim_desc,
+            triage: data[i].triage
+          }
+        );
+      }
+    }
   }
 
 }

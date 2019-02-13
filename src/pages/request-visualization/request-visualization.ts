b43d6a0be9a2a39febdddc7a9b168ce148672bad
unknown <SONY@DESKTOP-7LRTL6N.it4102.local>
2019-02-13 10:14:28 +0800

    maps

--- src/pages/request-visualization/request-visualization.ts
+++ src/pages/request-visualization/request-visualization.ts
@@ -149,7 +149,7 @@ export class RequestVisualizationPage {
         shadowSize: [41, 41]
       });  
       this.map = leaflet.map("map",{center:(this.currLat,this.currLong),  }).fitWorld();
-      leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18,
       }).addTo(this.map);

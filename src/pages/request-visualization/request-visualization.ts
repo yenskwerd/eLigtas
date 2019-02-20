aa9c776e16a355deef9fa674a6cda88b7e8ad2e4
unknown <SONY@DESKTOP-7LRTL6N.it4102.local>
2019-02-20 09:31:09 +0800

    zoom

--- src/pages/request-visualization/request-visualization.ts
+++ src/pages/request-visualization/request-visualization.ts
@@ -172,8 +172,8 @@ export class RequestVisualizationPage {
       this.map = leaflet.map("map").fitWorld();
       leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        maxZoom: 10,
-        minZoom:16
+        maxZoom: 14,
+        // minZoom:16
       }).addTo(this.map);
       this.map.locate({
         //center:(this.currLat,this.currLong),
